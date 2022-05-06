38622accbfabd890e648392c7796e80159b5ac71
Kirbitz <tyler.marefke@gmail.com>
2022-05-06 18:47:41 -0500

    Initial Setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import Express from 'express'
+
+// Creates a new Express app to run locally on a machine
+const app = new Express()
+
+// Tracks the requests made to the server
+app.use((req, res, next) => {
+  console.log(`${req.method} at ${req.path}`)
+  next()
+})
+
+// Serves the static page to the base URL
+app.use(Express.static('public'))
+
+// Connection created on "localhost:8000"
+app.listen(8000)
+console.log('Listening on port 8000')
