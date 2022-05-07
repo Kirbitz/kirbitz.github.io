520c2a36713d5a82b64c8e12f44e96be8722849f
Kirbitz <tyler.marefke@gmail.com>
2022-05-06 19:52:40 -0500

    Updated Toolbar with menu icon

--- client/components/PageHeader.jsx
+++ client/components/PageHeader.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, IconButton, Toolbar, Tooltip, Typography } from '@mui/material'
+import { Menu } from '@mui/icons-material' 
 
 export default function PageHeader (props) {
   return(
-    <AppBar>
+    <AppBar sx={{ bgcolor: '#6f49c9' }}>
       <Toolbar>
+        <Tooltip title='Open Menu'>
+          <IconButton
+            size='large'
+            edge='start'
+            color='inherit'
+            aria-label='menu-button'
+            sx={{ mr: 1 }}
+          >
+            <Menu />
+          </IconButton>
+        </Tooltip>
         <Typography variant='h3' component='h2'>
-          YOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+          Tyler Marefke Portfolio Website
         </Typography>
       </Toolbar>
     </AppBar>
