e04e260c334ac56bb105d162c68bed73456e4990
Kirbitz <tyler.marefke@gmail.com>
2023-04-19 15:37:15 -0500

    Added project and removed tech modal

--- client/components/homePage/HomeContent.jsx
+++ client/components/homePage/HomeContent.jsx
@@ -5,20 +5,9 @@ import { Divider, Typography } from '@mui/material'
 
 import Projects from './Projects.jsx'
 import Technologies from './Technologies.jsx'
-import IconsModal from './IconsModal.jsx'
 
 export default function HomeContent (props) {
   const { changePage } = props
-
-  const [showIconsModal, setShowIconsModal] = React.useState(false)
-
-  const openIconsModal = () => {
-    setShowIconsModal(true)
-  }
-
-  const hideModal = () => {
-    setShowIconsModal(false)
-  }
 
   return(
     <React.Fragment>
@@ -26,9 +15,8 @@ export default function HomeContent (props) {
         Portfolio
       </Typography>
       <Divider />
-      <Technologies openModal={openIconsModal} />
+      <Technologies />
       <Projects changePage={changePage} />
-      <IconsModal showModal={showIconsModal} onClose={hideModal} />
     </React.Fragment>
   )
 }
--- client/components/homePage/Technologies.jsx
+++ client/components/homePage/Technologies.jsx
@@ -1,23 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
+import { Avatar, Grid, Tooltip, Typography } from '@mui/material'
 
 import icons from '../../data/icons.json'
 
 export default function Technologies (props) {
-  const { openModal } = props
-
-  const hoverAction = {
-    cursor: 'pointer'
-  }
-
-  const avatars = icons.map((icon) => {
-    if(!['Java', 'JavaScript', 'Git', 'Python', 'React'].includes(icon.name)) {
-      return <Avatar key={icon.id} alt={`${icon.name}`} src={`${icon.link}`} variant='square' sx={{ mr: 2.5, ml: 2.5 }} />
-    } else {
-      return
-    }
+  const iconGrid = icons.map((icon) => {
+    return( 
+      <Grid item key={icon.id} xs={3} sm={2} lg={1}>
+        <Tooltip title={`${icon.name}`}>
+          <Avatar alt={`${icon.name}`} src={`${icon.link}`} variant="square" sx={{ mt: 1, mb: 1 }} />
+        </Tooltip>
+      </Grid>
+    )
   })
 
   return(
@@ -28,24 +24,9 @@ export default function Technologies (props) {
       <Typography variant='subtitle2' component='div' align='center' sx={{ mb: 2 }}>
         I Have Worked In
       </Typography>
-      <Box align='center' alignItems='center' justifyContent='center' onClick={openModal} sx={[{ display: 'flex' }, { '&:hover': hoverAction }]}>
-        <AvatarGroup max={6} spacing={1}>
-          <Avatar alt="Java" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" variant='square' sx={{ mr: 2.5, ml: 2.5 }} />
-          <Avatar alt="JavaScript" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" variant='square' sx={{ mr: 2.5, ml: 2.5 }} />
-          <Avatar alt="Git" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" variant='square' sx={{ mr: 2.5, ml: 2.5 }} />
-          <Avatar alt="Python" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" variant='square' sx={{ mr: 2.5, ml: 2.5 }} />
-          <Avatar alt="React" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" variant='square' sx={{ mr: 2.5, ml: 2.5 }} />
-          {avatars}
-        </AvatarGroup>
-      </Box>
+      <Grid container spacing={2} justifyContent='center' alignItems='center' align='center'>
+        {iconGrid}
+      </Grid>
     </React.Fragment>
   )
-}
-
-Technologies.propTypes = {
-  openModal: PropTypes.func
-}
-
-Technologies.defaultProps = {
-  openModal: null
 }
